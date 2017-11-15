6695fb2626dbf011a57fbec361404d7e7967a468
Thomas Burke <etburke4@gmail.com>
2017-11-15 14:25:13 -0800

    wrap in promise

--- client.js
+++ client.js
@@ -23,21 +23,23 @@ TrelloPowerUp.initialize({
       console.log('We just loaded the card name for fun: ' + cardId);
       return t.get(cardId, 'shared', 'votes', 0)
       .then(function(votes) {
-        return [{
-          // dynamic badges can have their function rerun
-          // after a set number of seconds defined by refresh.
-          // Minimum of 10 seconds.
-          dynamic: function(){
-            // we could also return a Promise that resolves to
-            // this as well if we needed to do something async first
-            return {
-              text: 'Votes ' + votes,
-              icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-              color: 'green',
-              refresh: 10 // in seconds
-            };
-          }
-        }];
+        return new Promise(function(resolve) {
+          resolve([{
+            // dynamic badges can have their function rerun
+            // after a set number of seconds defined by refresh.
+            // Minimum of 10 seconds.
+            dynamic: function(){
+              // we could also return a Promise that resolves to
+              // this as well if we needed to do something async first
+              return {
+                text: 'Votes ' + votes,
+                icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+                color: 'green',
+                refresh: 10 // in seconds
+              };
+            }
+          }]);
+        });
       });
     });
   }

e6a5f4fefce1cebd4e41c7468a983b0021260e85
Thomas Burke <etburke4@gmail.com>
2017-11-15 09:25:39 -0800

    Added callback

--- client.js
+++ client.js
@@ -1,8 +1,13 @@
+function addVoteToCard() {
+  console.log('addVoteToCard', arguments);
+}
+
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Add Dot Vote',
+      callback: addVoteToCard,
     }];
   },
   'card-badges': function (t, opts) {

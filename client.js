2fffe5747c3b59b2a41f8b5ccb907a136e91b188
Thomas Burke <etburke4@gmail.com>
2017-11-15 16:49:48 -0800

    cleanup

--- client.js
+++ client.js
@@ -15,14 +15,8 @@ TrelloPowerUp.initialize({
     return t.card('id')
     .get('id')
     .then(function(cardId) {
-      console.log('We just loaded the card name for fun: ' + cardId);
       return [{
-        // dynamic badges can have their function rerun
-        // after a set number of seconds defined by refresh.
-        // Minimum of 10 seconds.
         dynamic: function() {
-          // we could also return a Promise that resolves to
-          // this as well if we needed to do something async first
           return t.get(cardId, 'shared', 'votes', 0)
           .then(function(votes) {
             return {

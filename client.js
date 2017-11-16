8f2ab9bd6d6695ed0bd365543965e0582376bff2
Thomas Burke <etburke4@gmail.com>
2017-11-15 16:20:27 -0800

    trying the async dynamic badge

--- client.js
+++ client.js
@@ -19,28 +19,26 @@ TrelloPowerUp.initialize({
   'card-badges': function (t, opts) {
     return t.card('name')
     .get('id')
-    .then(function(cardId){
+    .then(function(cardId) {
       console.log('We just loaded the card name for fun: ' + cardId);
-      return t.get(cardId, 'shared', 'votes', 0)
-      .then(function(votes) {
-        return new Promise(function(resolve) {
-          resolve([{
-            // dynamic badges can have their function rerun
-            // after a set number of seconds defined by refresh.
-            // Minimum of 10 seconds.
-            dynamic: function(){
-              // we could also return a Promise that resolves to
-              // this as well if we needed to do something async first
-              return {
-                text: 'Votes ' + votes,
-                icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-                color: 'green',
-                refresh: 10 // in seconds
-              };
-            }
-          }]);
-        });
-      });
+      return [{
+        // dynamic badges can have their function rerun
+        // after a set number of seconds defined by refresh.
+        // Minimum of 10 seconds.
+        dynamic: function() {
+          // we could also return a Promise that resolves to
+          // this as well if we needed to do something async first
+          return t.get(cardId, 'shared', 'votes', 0)
+          .then(function(votes) {
+            return {
+              text: 'Votes ' + votes,
+              icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+              color: 'green',
+              refresh: 10 // in seconds
+            };
+          });
+        },
+      }];
     });
-  }
+  },
 });

72814ebd6630f88d4e1ca73627150176c6c8bbcb
Thomas Burke <etburke4@gmail.com>
2017-12-17 10:25:03 -0800

    3 votes per member

--- vote.js
+++ vote.js
@@ -6,7 +6,21 @@ window.add.addEventListener('click', function(event) {
 
   return t.get(context.card, 'shared', 'votes', 0)
   .then(function(votes) {
-    return t.set(context.card, 'shared', 'votes', (votes + 1));
+    return t.get(context.member, 'shared', 'votes', 0)
+    .then(function (votesCast) {
+      console.log('votesCast', votesCast);
+      if (votesCast < 3) {
+        return votes;
+      }
+    });
+  })
+  .then(function(votes) {
+    if (votes) {
+      return t.set(context.card, 'shared', 'votes', (votes + 1))
+      .then(function() {
+        return t.set(context.member, 'shared', 'votes', (votes + 1));
+      });
+    }
   });
 });
 
@@ -15,7 +29,21 @@ window.remove.addEventListener('click', function(event) {
 
   return t.get(context.card, 'shared', 'votes', 0)
   .then(function(votes) {
-    return t.set(context.card, 'shared', 'votes', (votes ? votes - 1 : votes));
+    return t.get(context.member, 'shared', 'votes', 0)
+    .then(function (votesCast) {
+      console.log('votesCast', votesCast);
+      if (votesCast >= 3) {
+        return votes;
+      }
+    });
+  })
+  .then(function(votes) {
+    if (votes) {
+      return t.set(context.card, 'shared', 'votes', (votes ? votes - 1 : votes))
+      .then(function() {
+        return t.set(context.member, 'shared', 'votes', (votes + 1));
+      });
+    }
   });
 });
 

dd74eab431181fe662e5e3498ff084e7b42ef518
Thomas Burke <etburke4@gmail.com>
2017-12-17 10:55:22 -0800

    starting reset all votes

--- vote.js
+++ vote.js
@@ -41,9 +41,18 @@ window.remove.addEventListener('click', function(event) {
     if (votes) {
       return t.set(context.card, 'shared', 'votes', (votes ? votes - 1 : votes))
       .then(function() {
-        return t.set('member', 'shared', 'votes', (votes + 1));
+        return t.set('member', 'shared', 'votes', (votes ? votes - 1 : votes));
       });
     }
+  });
+});
+
+window.reset.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  return t.getAll()
+  .then(function(allData) {
+    console.log('allData', allData);
   });
 });
 

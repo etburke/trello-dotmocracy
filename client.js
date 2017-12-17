97c3e5fed2f25713306c757733041321c560e0c6
Thomas Burke <etburke4@gmail.com>
2017-12-17 07:38:24 -0800

    added card detail badges

--- client.js
+++ client.js
@@ -30,4 +30,23 @@ TrelloPowerUp.initialize({
       }];
     });
   },
+  'card-detail-badges': function (t, opts) {
+    return t.card('id')
+    .get('id')
+    .then(function(cardId) {
+      return [{
+        dynamic: function() {
+          return t.get(cardId, 'shared', 'votes', 0)
+          .then(function(votes) {
+            return {
+              text: 'Votes ' + votes,
+              icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+              color: 'green',
+              refresh: 10 // in seconds
+            };
+          });
+        },
+      }];
+    });
+  },
 });

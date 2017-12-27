5dd4f980d15a1791b8ea416766c4b762c1204a6e
Thomas Burke <etburke4@gmail.com>
2017-12-27 16:46:26 -0500

    added babel polyfill

--- src/client.js
+++ src/client.js
@@ -1,7 +1,10 @@
+require('babel-core/register');
+require('babel-polyfill');
+
 const cardButtons = () => ([{
   icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
   text: 'Dotmocratize',
-  callback: tt => tt.popup({
+  callback: t => t.popup({
     title: 'Vote',
     url: 'vote.html',
   }),

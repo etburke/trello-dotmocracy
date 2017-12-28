c9a2d6bd1528d9b39e4cae0caabf5251b09fdeda
Thomas Burke <etburke4@gmail.com>
2017-12-28 11:02:42 -0500

    move votes remaining

--- src/capabilities/card-badges/index.js
+++ src/capabilities/card-badges/index.js
@@ -1,9 +1,7 @@
 import votes from './votes';
-import votesRemaining from './votes-remaining';
 
 const cardBadges = async t => ([
   await votes(t),
-  await votesRemaining(t),
 ]);
 
 export default cardBadges;
--- src/capabilities/card-detail-badges/index.js
+++ src/capabilities/card-detail-badges/index.js
@@ -1,7 +1,9 @@
 import votes from '../card-badges/votes';
+import votesRemaining from './votes-remaining';
 
 const cardDetailBadges = async t => ([
   await votes(t),
+  await votesRemaining(t),
 ]);
 
 export default cardDetailBadges;

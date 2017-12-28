baf8be2266221376040458911b0457ae41f1bc9f
Thomas Burke <etburke4@gmail.com>
2017-12-27 19:05:48 -0500

    adding board buttons

--- src/client.js
+++ src/client.js
@@ -1,6 +1,17 @@
 import 'babel-core/register';
 import 'babel-polyfill';
 
+const boardButtons = () => ([
+  {
+    icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+    text: 'Reset Votes',
+  },
+  {
+    icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+    text: 'Set Timer',
+  },
+]);
+
 const cardButtons = () => ([{
   icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
   text: 'Dotmocratize',
@@ -46,6 +57,7 @@ const cardDetailBadges = async (t) => {
 };
 
 TrelloPowerUp.initialize({
+  'board-buttons': boardButtons,
   'card-buttons': cardButtons,
   'card-badges': cardBadges,
   'card-detail-badges': cardDetailBadges,

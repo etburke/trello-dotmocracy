061705f65c28aa2e855c350d88dd685c5f0c6593
Thomas Burke <etburke4@gmail.com>
2017-12-29 11:53:41 -0500

    reset

--- src/capabilities/board-buttons/reset-votes.js
+++ src/capabilities/board-buttons/reset-votes.js
@@ -1,9 +1,15 @@
+const reset = t => async item => await t.set('board', 'shared', item, 0);
+
 const resetVotes = {
   icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
   text: 'Reset Votes',
   callback: async (t) => {
-    const allData = await t.getAll();
-    console.log('allData', allData);
+    const { board } = await t.getAll();
+    const promises = Object.keys(board)
+      .filter(i => i.includes('votes'))
+      .map(i => reset(t)(i));
+
+    await Promise.all(promises);
   },
 };
 

556cea85ca74ef4934ebb597423e5a8f3f67e8ab
Thomas Burke <etburke4@gmail.com>
2017-12-29 12:13:32 -0500

    debugging

--- src/capabilities/board-buttons/reset-votes.js
+++ src/capabilities/board-buttons/reset-votes.js
@@ -1,9 +1,11 @@
-const reset = t => async item => await t.set('board', 'shared', item, 0);
-
 const resetVotes = {
   icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
   text: 'Reset Votes',
   callback: async (t) => {
+    const reset = async (item) => {
+      console.log('resetting item', item);
+      await t.set('board', 'shared', item, 0);
+    };
     const { board } = await t.getAll();
     console.log('board', board);
     const promises = Object.keys(board.shared)

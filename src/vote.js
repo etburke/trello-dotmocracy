13df9162c92f72fd53d9f4b2594f4b2f471a20ba
Thomas Burke <etburke4@gmail.com>
2017-12-29 14:19:39 -0500

    fixing remove logic

--- src/vote.js
+++ src/vote.js
@@ -29,7 +29,7 @@ window.remove.addEventListener('click', async (event) => {
     const votesCast = await t.get('board', 'shared', `${context.member}:votes`, 0);
     console.log('votes', votes);
     console.log('votesCast', votesCast);
-    if (votes != null && votesCast <= 3 && votesCast > 0) {
+    if (votes && votesCast <= 3 && votesCast > 0) {
       await t.set('board', 'shared', `${context.card}:votes`, (votes ? votes - 1 : votes));
       await t.set('board', 'shared', `${context.member}:votes`, (votesCast ? votesCast - 1 : votesCast));
     }

e30a58e3326055f95fb82d4c809e721e797a56fe
Thomas Burke <etburke4@gmail.com>
2017-12-29 14:03:40 -0500

    back to a/a

--- src/capabilities/board-buttons/reset-votes.js
+++ src/capabilities/board-buttons/reset-votes.js
@@ -1,13 +1,12 @@
 const resetVotes = {
   icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
   text: 'Reset Votes',
-  callback: t => t.getAll()
-    .then(({ board }) => {
-      const items = Object.keys(board.shared)
-        .filter(i => i.includes('votes'));
-      return t.remove('board', 'shared', items);
-    })
-    .then(res => console.log('res', res)),
+  callback: async (t) => {
+    const { board } = await t.getAll();
+    const items = Object.keys(board.shared)
+      .filter(i => i.includes('votes'));
+    await t.remove('board', 'shared', items);
+  },
 };
 
 export default resetVotes;

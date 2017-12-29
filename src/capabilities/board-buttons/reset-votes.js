7f514e014dda358f32f36eff7b1edcf773a29292
Thomas Burke <etburke4@gmail.com>
2017-12-29 12:20:46 -0500

    try/catch

--- src/capabilities/board-buttons/reset-votes.js
+++ src/capabilities/board-buttons/reset-votes.js
@@ -4,15 +4,23 @@ const resetVotes = {
   callback: async (t) => {
     const reset = async (item) => {
       console.log('resetting item', item);
-      await t.set('board', 'shared', item, 0);
+      try {
+        await t.set('board', 'shared', item, 0);
+      } catch (ex) {
+        console.log('ex', ex);
+      }
     };
-    const { board } = await t.getAll();
-    console.log('board', board);
-    const promises = Object.keys(board.shared)
-      .filter(i => i.includes('votes'))
-      .map(i => reset(i));
-    console.log('promises', promises);
-    await Promise.all(promises);
+    try {
+      const { board } = await t.getAll();
+      console.log('board', board);
+      const promises = Object.keys(board.shared)
+        .filter(i => i.includes('votes'))
+        .map(i => reset(i));
+      console.log('promises', promises);
+      await Promise.all(promises);
+    } catch (ex) {
+      console.log('Promise.all ex', ex);
+    }
   },
 };
 
